b6eff5d7a9847dd24cdf9259e50149fcfa2635c9
lyrithy <lyrithyit@gmail.com>
2018-10-05 00:02:12 +0200

    ccy convertor build success

--- currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/currency/CurrencyConvertorImpl.java
@@ -1,18 +1,17 @@
 package cz.muni.fi.pa165.currency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
 
 
-/**
- * This is base implementation of {@link CurrencyConvertor}.
- *
- * @author petr.adamek@embedit.cz
- */
 public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     private final ExchangeRateTable exchangeRateTable;
-    //private final Logger logger = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
+    private final static Logger log = LoggerFactory.getLogger(CurrencyConvertorImpl.class);
 
     public CurrencyConvertorImpl(ExchangeRateTable exchangeRateTable) {
         this.exchangeRateTable = exchangeRateTable;
@@ -20,8 +19,29 @@ public class CurrencyConvertorImpl implements CurrencyConvertor {
 
     @Override
     public BigDecimal convert(Currency sourceCurrency, Currency targetCurrency, BigDecimal sourceAmount) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        log.trace("convert({},{},{})",sourceCurrency, targetCurrency, sourceAmount);
+        //
+        if (sourceCurrency == null) {
+            throw new IllegalArgumentException("sourceCurrency null");
+        }
+        if (targetCurrency == null) {
+            throw new IllegalArgumentException("targetCurrency null");
+        }
+        if (sourceAmount == null) {
+            throw new IllegalArgumentException("sourceAmount null");
+        }
+        
+        
+        try {
+            BigDecimal exchangeRate = exchangeRateTable.getExchangeRate(sourceCurrency, targetCurrency);
+            if (exchangeRate == null) {
+                throw new UnknownExchangeRateException("ExchangeRate unknown");
+            }
+            //TODO: result of ccy convertor
+            return exchangeRate.multiply(sourceAmount).setScale(2, RoundingMode.HALF_EVEN);
+        } catch (ExternalServiceFailureException ex) {
+            throw new UnknownExchangeRateException("Error when fetching exchange rate", ex);
+        }
     }
 
-    
 }
